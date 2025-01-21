125481fa3bfc11ba1b75c3dec49eeef6383da69d
billah1 <billah9578@gmail.com>
2025-01-22 01:01:54 +0600

    user and comment

--- database/migrations/2025_01_21_164426_create_comments_table.php
+++ database/migrations/2025_01_21_164426_create_comments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('blog_id')->constrained('blogs')->onDelete('cascade');
+            $table->foreignId('blog_id')->onDelete('cascade');
             $table->text('title');
             $table->timestamps();
         });
